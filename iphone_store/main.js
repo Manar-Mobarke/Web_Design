b07afd35ec030c8e378ed022380526303d9afee0
Manar-Mobarke <81156136+Manar-Mobarke@users.noreply.github.com>
2023-07-11 14:43:11 +0300

    Add files via upload

---
+++ iphone_store/main.js
@@ -0,0 +1,12 @@
+let img= document.querySelector('.img')
+
+function phones(phone){
+    img.src = phone;
+
+}
+
+let container=document.querySelector('.container')
+
+function colors(color){
+    container.style.background = color;
+}
